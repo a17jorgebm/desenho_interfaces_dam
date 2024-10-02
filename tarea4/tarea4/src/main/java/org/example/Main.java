84c1aa1aa1c35a29d996fc3484b8e54f67f0fe10
jorge <a17jorgebm@iessanclemente.net>
2024-10-02 11:15:19 +0200

    ejer4 sin acabar

---
+++ tarea4/tarea4/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static final String COLOR_ROJO="";
+    public static final String COLOR_AZUL="";
+    public static void main(String[] args) {
+
+    }
+    public static void cambiarValoresGBC(){
+        
+    }
+}
