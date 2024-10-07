91980652bb04e4ae1c2c15c8fc965fd64e0bef0a
jorge <a17jorgebm@iessanclemente.net>
2024-10-07 14:34:53 +0200

    ejer4

--- tarea4/tarea4/src/main/java/org/example/Main.java
+++ tarea4/tarea4/src/main/java/org/example/Main.java
@@ -2,13 +2,16 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class Main {
     public static final String COLOR_ROJO="#cb5b5e";
     public static final String COLOR_AZUL="#7abcde";
+    public static final int MARGIN_ENTRE_ELEMENTOS=10;
     public static void main(String[] args) {
         JFrame ventana=new JFrame();
-        ventana.setMinimumSize(new Dimension(300,200));
+        ventana.setMinimumSize(new Dimension(600,400));
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLayout(new GridBagLayout());
 
@@ -23,36 +26,38 @@ public class Main {
 
             JPanel header=new JPanel();
             header.setLayout(new GridBagLayout());
-            header.setBackground(Color.getColor(COLOR_ROJO));
-            cambiarValoresGBC(gbc,0,0,1,0.2);
+            header.setBackground(Color.decode(COLOR_ROJO));
             header.add(new JLabel("cabeceira"));
+            cambiarValoresGBC(gbc,0,0,1,0.1);
             main.add(header,gbc);
 
             JPanel centro=new JPanel();
             centro.setLayout(new GridBagLayout());
-            insets.set(15,0,15,0);
-            cambiarValoresGBC(gbc,0,1,1,0.7,insets);
+            insets.set(MARGIN_ENTRE_ELEMENTOS,0,MARGIN_ENTRE_ELEMENTOS,0);
+            cambiarValoresGBC(gbc,0,1,1,0.85,insets);
             main.add(centro,gbc);
 
                 JPanel esquerda=new JPanel();
                 esquerda.setLayout(new GridBagLayout());
-                esquerda.setBackground(Color.decode(COLOR_ROJO));
+                esquerda.setBackground(Color.decode(COLOR_AZUL));
                 esquerda.add(new JLabel("menu de navegación"));
-                insets.set(15,0,15,0);
-                cambiarValoresGBC(gbc,0,0,0.3,1,insets);
+                insets.set(0,0,0,MARGIN_ENTRE_ELEMENTOS);
+                cambiarValoresGBC(gbc,0,0,0.2,1,1,1,GridBagConstraints.BOTH,insets);
                 centro.add(esquerda,gbc);
 
                 JPanel dereita=new JPanel();
                 dereita.setLayout(new GridBagLayout());
                 dereita.setBackground(Color.decode(COLOR_ROJO));
-                cambiarValoresGBC(gbc,1,0,0.7,1);
+                dereita.add(new JLabel("menu de navegación"));
+
+                cambiarValoresGBC(gbc,1,0,0.8,1,3,1,GridBagConstraints.BOTH);
                 centro.add(dereita,gbc);
 
             JPanel footer=new JPanel();
             footer.setLayout(new GridBagLayout());
             footer.setBackground(Color.decode(COLOR_ROJO));
             footer.add(new JLabel("pé"));
-            cambiarValoresGBC(gbc,0,2,1,0.1);
+            cambiarValoresGBC(gbc,0,2,1,0.05);
             main.add(footer,gbc);
 
         ventana.revalidate();
@@ -97,5 +102,15 @@ public class Main {
         objetoConstraints.fill=fill;
         //reseteanse os insets
         objetoConstraints.insets=new Insets(0,0,0,0);
+    }
+    public static void cambiarValoresGBC(GridBagConstraints objetoConstraints,int gridx,int gridy,double weightx,double weighty,int gridwidth,int gridheight,int fill, Insets insets){
+        objetoConstraints.gridx=gridx;
+        objetoConstraints.gridy=gridy;
+        objetoConstraints.weightx=weightx;
+        objetoConstraints.weighty=weighty;
+        objetoConstraints.gridwidth=gridwidth;
+        objetoConstraints.gridheight=gridheight;
+        objetoConstraints.fill=fill;
+        objetoConstraints.insets=insets;
     }
 }
