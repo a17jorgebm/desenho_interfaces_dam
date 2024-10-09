a18d7ba094407ccd254e94e9dc2bc45779b5589c
Jorge Blanco Mariño <a17jorgebm@iessanclemente.net>
2024-10-09 10:44:21 +0200

    posicionamiento v2

--- tarea4/tarea4/src/main/java/org/example/EstructuraWebBasicav2.java
+++ tarea4/tarea4/src/main/java/org/example/EstructuraWebBasicav2.java
@@ -1,17 +1,17 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 
-public class Main {
+public class EstructuraWebBasicav2 {
     public static final String COLOR_ROJO="#cb5b5e";
     public static final String COLOR_AZUL="#7abcde";
+    public static final String COLOR_TEXTO="#FFFFFF";
     public static final int MARGIN_ENTRE_ELEMENTOS=10;
     public static void main(String[] args) {
         JFrame ventana=new JFrame();
-        ventana.setMinimumSize(new Dimension(600,400));
+        ventana.setMinimumSize(new Dimension(800,600));
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setLayout(new GridBagLayout());
 
@@ -27,7 +27,7 @@ public class Main {
             JPanel header=new JPanel();
             header.setLayout(new GridBagLayout());
             header.setBackground(Color.decode(COLOR_ROJO));
-            header.add(new JLabel("cabeceira"));
+            header.add(crearTexto("cabeceira"));
             cambiarValoresGBC(gbc,0,0,1,0.1);
             main.add(header,gbc);
 
@@ -39,8 +39,8 @@ public class Main {
 
                 JPanel esquerda=new JPanel();
                 esquerda.setLayout(new GridBagLayout());
-                esquerda.setBackground(Color.decode(COLOR_AZUL));
-                esquerda.add(new JLabel("menu de navegación"));
+                esquerda.setBackground(Color.decode(COLOR_ROJO));
+                esquerda.add(crearTexto("menu de navegación"));
                 insets.set(0,0,0,MARGIN_ENTRE_ELEMENTOS);
                 cambiarValoresGBC(gbc,0,0,0.2,1,1,1,GridBagConstraints.BOTH,insets);
                 centro.add(esquerda,gbc);
@@ -48,15 +48,36 @@ public class Main {
                 JPanel dereita=new JPanel();
                 dereita.setLayout(new GridBagLayout());
                 dereita.setBackground(Color.decode(COLOR_ROJO));
-                dereita.add(new JLabel("menu de navegación"));
-
+                dereita.setBorder(new EmptyBorder(20,MARGIN_ENTRE_ELEMENTOS,50,MARGIN_ENTRE_ELEMENTOS));
                 cambiarValoresGBC(gbc,1,0,0.8,1,3,1,GridBagConstraints.BOTH);
                 centro.add(dereita,gbc);
+
+                    JPanel derHeader=new JPanel();
+                    derHeader.setLayout(new GridBagLayout());
+                    derHeader.setBackground(Color.decode(COLOR_AZUL));
+                    derHeader.add(crearTexto("artigo"));
+                    cambiarValoresGBC(gbc,0,0,1,0.1,1,1,GridBagConstraints.BOTH);
+                    dereita.add(derHeader,gbc);
+
+                    JPanel derCentro=new JPanel();
+                    derCentro.setLayout(new GridBagLayout());
+                    derCentro.setBackground(Color.decode(COLOR_AZUL));
+                    derCentro.add(crearTexto("artigo"));
+                    insets.set(MARGIN_ENTRE_ELEMENTOS,0,MARGIN_ENTRE_ELEMENTOS,0);
+                    cambiarValoresGBC(gbc,0,1,1,0.7,1,7,GridBagConstraints.BOTH,insets);
+                    dereita.add(derCentro,gbc);
+
+                    JPanel derFooter=new JPanel();
+                    derFooter.setLayout(new GridBagLayout());
+                    derFooter.setBackground(Color.decode(COLOR_AZUL));
+                    derFooter.add(crearTexto("artigo"));
+                    cambiarValoresGBC(gbc,0,8,1,0.2,1,3,GridBagConstraints.BOTH);
+                    dereita.add(derFooter,gbc);
 
             JPanel footer=new JPanel();
             footer.setLayout(new GridBagLayout());
             footer.setBackground(Color.decode(COLOR_ROJO));
-            footer.add(new JLabel("pé"));
+            footer.add(crearTexto("pé"));
             cambiarValoresGBC(gbc,0,2,1,0.05);
             main.add(footer,gbc);
 
@@ -112,5 +133,12 @@ public class Main {
         objetoConstraints.gridheight=gridheight;
         objetoConstraints.fill=fill;
         objetoConstraints.insets=insets;
+    }
+
+    public static JLabel crearTexto(String texto){
+        JLabel label=new JLabel(texto);
+        label.setForeground(Color.decode(COLOR_TEXTO));
+        label.setFont(new Font("Times New Roman",Font.BOLD,15));
+        return label;
     }
 }
