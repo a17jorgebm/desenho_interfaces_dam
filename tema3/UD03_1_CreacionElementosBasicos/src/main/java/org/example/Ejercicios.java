ff334e576f3b5737967a29c6831a4af7aad60a6d
jorge <a17jorgebm@iessanclemente.net>
2024-11-13 17:40:16 +0100

    ejer9 sin acabar

--- tema3/UD03_1_CreacionElementosBasicos/src/main/java/org/example/Ejercicios.java
+++ tema3/UD03_1_CreacionElementosBasicos/src/main/java/org/example/Ejercicios.java
@@ -5,6 +5,10 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Ejercicios {
     public static JFrame ejer1(){
@@ -82,18 +86,43 @@ public class Ejercicios {
 
 
         JLabel labelNombre=new JLabel("Nombre:");
-        JTextField textFieldNombre = new JTextField();
         Funcions.cambiarValoresGBC(gbc,0,0,0.5,1,new Insets(0,0,0,15));
-        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.fill=GridBagConstraints.NONE;
         gbc.anchor=GridBagConstraints.LINE_END;
         panelCompleto.add(labelNombre,gbc);
         Funcions.cambiarValoresGBC(gbc,1,0,1,1);
+        JTextField textFieldNombre = new JTextField();
         gbc.fill=GridBagConstraints.HORIZONTAL;
         gbc.anchor=GridBagConstraints.CENTER;
         panelCompleto.add(textFieldNombre,gbc);
 
         JLabel labelApellidos=new JLabel("Apellidos:");
+        Funcions.cambiarValoresGBC(gbc,0,1,0.5,1,new Insets(0,0,0,15));
+        gbc.fill=GridBagConstraints.NONE;
+        gbc.anchor=GridBagConstraints.LINE_END;
+        panelCompleto.add(labelApellidos,gbc);
+        Funcions.cambiarValoresGBC(gbc,1,1,1,1);
         JTextField textFieldApellidos = new JTextField();
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.anchor=GridBagConstraints.CENTER;
+        panelCompleto.add(textFieldApellidos,gbc);
+
+        JButton btnEnviar=new JButton("Enviar");
+        Funcions.cambiarValoresGBC(gbc,0,2,1,1);
+        panelCompleto.add(btnEnviar,gbc);
+
+        btnEnviar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String textoNombre=textFieldNombre.getText().trim();
+                String textoApellidos=textFieldApellidos.getText().trim();
+                if (textoNombre.isBlank() || textoApellidos.isBlank()){
+                    System.out.println("Completa el nombre y apellidos para enviar");
+                }else {
+                    System.out.printf("Listo! Tu nombre es %s %s", textoNombre, textoApellidos);
+                }
+            }
+        });
 
         return ventana;
     }
@@ -101,17 +130,92 @@ public class Ejercicios {
         JFrame ventana=Funcions.getGentanaBasica();
         ventana.setTitle("Ejer 5");
 
+        JButton botonMostrarDialogo=new JButton("Mostrar dialogo");
+        botonMostrarDialogo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String edad=JOptionPane.showInputDialog(ventana,"Introduce tu edad").trim();
+                if (!edad.isBlank()){
+                    JOptionPane.showMessageDialog(ventana,edad);
+                }
+                else {
+                    JOptionPane.showMessageDialog(ventana,"No has introducido nada");
+                }
+            }
+        });
+
+        ventana.add(botonMostrarDialogo);
+
         return ventana;
     }
     public static JFrame ejer6(){
         JFrame ventana=Funcions.getGentanaBasica();
         ventana.setTitle("Ejer 6");
 
+        String[] colores= {"Rojo", "Verde", "Azul"};
+        JComboBox<String> comboBox=new JComboBox<>(colores);
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(comboBox.getSelectedItem());
+            }
+        });
+
+        ventana.add(comboBox);
         return ventana;
     }
     public static JFrame ejer7(){
         JFrame ventana=Funcions.getGentanaBasica();
         ventana.setTitle("Ejer 7");
+
+        GridBagConstraints gbc=new GridBagConstraints();
+
+        JPanel panelCompleto=new JPanel();
+        panelCompleto.setLayout(new GridBagLayout());
+        panelCompleto.setBorder(new EmptyBorder(45,45,45,45));
+        Funcions.cambiarValoresGBC(gbc,0,0,0.5,1);
+        gbc.fill=GridBagConstraints.BOTH;
+        ventana.add(panelCompleto,gbc);
+
+
+        JLabel labelNombre=new JLabel("Nombre:");
+        Funcions.cambiarValoresGBC(gbc,0,0,0.5,1,new Insets(0,0,0,15));
+        gbc.fill=GridBagConstraints.NONE;
+        gbc.anchor=GridBagConstraints.LINE_END;
+        panelCompleto.add(labelNombre,gbc);
+        Funcions.cambiarValoresGBC(gbc,1,0,1,1);
+        JTextField textFieldNombre = new JTextField();
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.anchor=GridBagConstraints.CENTER;
+        panelCompleto.add(textFieldNombre,gbc);
+
+        JLabel labelContrasinal=new JLabel("Contraseña:");
+        Funcions.cambiarValoresGBC(gbc,0,1,0.5,1,new Insets(0,0,0,15));
+        gbc.fill=GridBagConstraints.NONE;
+        gbc.anchor=GridBagConstraints.LINE_END;
+        panelCompleto.add(labelContrasinal,gbc);
+        Funcions.cambiarValoresGBC(gbc,1,1,1,1);
+        JPasswordField textFieldContrasena = new JPasswordField();
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.anchor=GridBagConstraints.CENTER;
+        panelCompleto.add(textFieldContrasena,gbc);
+
+        JButton btnEnviar=new JButton("Iniciar sesión");
+        Funcions.cambiarValoresGBC(gbc,0,2,1,1);
+        panelCompleto.add(btnEnviar,gbc);
+
+        btnEnviar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String textoNombre=textFieldNombre.getText().trim();
+                String textoContrasena=textFieldContrasena.getText().trim();
+                if (textoNombre.isBlank() || textoContrasena.isBlank()){
+                    System.out.println("Completa el nombre y contraseña para iniciar sesión");
+                }else {
+                    System.out.printf("Listo! Tu nombre es %s con contraseña %s", textoNombre, textoContrasena);
+                }
+            }
+        });
 
         return ventana;
     }
@@ -119,11 +223,89 @@ public class Ejercicios {
         JFrame ventana=Funcions.getGentanaBasica();
         ventana.setTitle("Ejer 8");
 
+        GridBagConstraints gbc=new GridBagConstraints();
+
+        JPanel panelCompleto=new JPanel();
+        panelCompleto.setLayout(new GridBagLayout());
+        panelCompleto.setBorder(new EmptyBorder(45,45,45,45));
+        Funcions.cambiarValoresGBC(gbc,0,0,0.5,1);
+        gbc.fill=GridBagConstraints.BOTH;
+        ventana.add(panelCompleto,gbc);
+
+        Funcions.cambiarValoresGBC(gbc,0,0,1,1);
+        JRadioButton radioTarjeta=new JRadioButton("Tarjeta de crédito");
+        radioTarjeta.setActionCommand("Tarjeta de crédito");
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.anchor=GridBagConstraints.CENTER;
+        panelCompleto.add(radioTarjeta,gbc);
+
+        Funcions.cambiarValoresGBC(gbc,0,1,1,1);
+        JRadioButton radioPaypal=new JRadioButton("PayPal");
+        radioPaypal.setActionCommand("PayPal");
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.anchor=GridBagConstraints.CENTER;
+        panelCompleto.add(radioPaypal,gbc);
+
+        Funcions.cambiarValoresGBC(gbc,0,2,1,1);
+        JRadioButton radioTransferencia=new JRadioButton("Transferencia bancaria");
+        radioTransferencia.setActionCommand("Transferencia bancaria");
+        gbc.fill=GridBagConstraints.HORIZONTAL;
+        gbc.anchor=GridBagConstraints.CENTER;
+        panelCompleto.add(radioTransferencia,gbc);
+
+        ButtonGroup grupo=new ButtonGroup();
+        grupo.add(radioTarjeta);
+        grupo.add(radioPaypal);
+        grupo.add(radioTransferencia);
+
+        JButton button=new JButton("Confirmar");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(grupo.getSelection().getActionCommand());
+            }
+        });
+        Funcions.cambiarValoresGBC(gbc,0,3,1,1);
+        panelCompleto.add(button,gbc);
+
         return ventana;
     }
     public static JFrame ejer9(){
         JFrame ventana=Funcions.getGentanaBasica();
         ventana.setTitle("Ejer 9");
+
+        List<Checkbox> checkboxes=new ArrayList<>();
+
+        GridBagConstraints gbc=new GridBagConstraints();
+
+        JPanel panelCompleto=new JPanel();
+        panelCompleto.setLayout(new GridBagLayout());
+        panelCompleto.setBorder(new EmptyBorder(45,45,45,45));
+        Funcions.cambiarValoresGBC(gbc,0,0,0.5,1);
+        gbc.fill=GridBagConstraints.BOTH;
+        ventana.add(panelCompleto,gbc);
+
+        String[] toppings={"Queso","Extra","Pepperoni","Aceitunas"};
+
+        for (int i=0;i<toppings.length;i++){
+            Funcions.cambiarValoresGBC(gbc,0,i,1,1);
+            JCheckBox checkBox=new JCheckBox(toppings[i]);
+            gbc.fill=GridBagConstraints.HORIZONTAL;
+            gbc.anchor=GridBagConstraints.CENTER;
+            panelCompleto.add(checkBox,gbc);
+        }
+
+        //seguir esto
+
+        JButton button=new JButton("Confirmar");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(grupo.getSelection().getActionCommand());
+            }
+        });
+        Funcions.cambiarValoresGBC(gbc,0,3,1,1);
+        panelCompleto.add(button,gbc);
 
         return ventana;
     }
