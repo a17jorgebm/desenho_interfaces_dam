3ef0d8c38d4d4a62cc9e94edab94b91c83081551
jorge <a17jorgebm@iessanclemente.net>
2024-11-14 09:03:42 +0100

    FlatLaf funcionando

--- tema3/UD_03_5_PRUEBA_Editor_Texto/src/main/java/org/example/Main.java
+++ tema3/UD_03_5_PRUEBA_Editor_Texto/src/main/java/org/example/Main.java
@@ -13,8 +13,14 @@ public class Main {
         }
 
         JFrame frame = new JFrame("Custom Look and Feel");
+        frame.setLocationRelativeTo(null);
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar barra=new JMenuBar();
+        JMenu accion=new JMenu("Accion");
+        barra.add(accion);
+        frame.setJMenuBar(barra);
 
         JTextArea textArea = new JTextArea("This JFrame uses FlatLaf for a custom look and feel.");
         frame.add(new JScrollPane(textArea));
