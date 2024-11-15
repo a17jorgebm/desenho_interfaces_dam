b3903608b1a1a95274f672caf93a94c6cd67b3fd
Jorge Blanco Mariño <a17jorgebm@iessanclemente.net>
2024-11-15 19:07:22 +0100

    Actualizacion do nome da ventana, gardado correcto. Falta ler correctamente os saltos de linea, e mostrar errores

--- tema3/UD_03_5_PRUEBA_Editor_Texto/src/main/java/org/example/Main.java
+++ tema3/UD_03_5_PRUEBA_Editor_Texto/src/main/java/org/example/Main.java
@@ -4,20 +4,20 @@ import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Main {
+    private static final String[] VALID_TEXT_FILE_EXTENSIONS = {".txt", ".html", ".htm", ".md", ".json", ".xml", ".csv", ".log", ".css", ".js", ".java", ".sql", ".yaml", ".yml", ".php", ".rb", ".py", ".pl", ".cpp", ".h", ".c", ".swift", ".go", ".scala", ".tex", ".rtf", ".ini", ".conf", ".properties", ".bat", ".sh", ".bash", ".ts", ".twig", ".mdx", ".asciidoc", ".rst", ".htaccess"};
     private static final String ICON_URL_LIGHT_DARK="/icons/night-light.png";
     private static final int OPTION_CANCEL_OPERATION=0;
     private static final int OPTION_SAVE_FILE=1;
@@ -34,7 +34,7 @@ public class Main {
     public boolean fileContentHasBeenEdited;
 
     public Main(){
-        this.ventana=new JFrame("Editor de texto");
+        this.ventana=new JFrame();
         this.layeredPane=ventana.getLayeredPane();
         this.gbc=new GridBagConstraints();
         this.textAreaEditor=new JTextArea();
@@ -56,17 +56,18 @@ public class Main {
                 e.printStackTrace();
             }
 
-            ventana.setLocationRelativeTo(null);
             ventana.setSize(800, 600);
             ventana.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            ventana.validate(); //to apply the window size so when I do setLocation it works properly
+            ventana.setLocationRelativeTo(null);
             ventana.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosing(WindowEvent e) {
-                    if (!Main.this.fileContentHasBeenEdited){ //luis non me mates :)
+                    if (Main.this.fileContentHasBeenEdited){ //luis non me mates :)
                         int actionToDo = askIfSaveBeforeDoAction();
                         if (actionToDo==OPTION_CANCEL_OPERATION) return;
                         if (actionToDo==OPTION_SAVE_FILE) {
-                            if(!saveTextFileContents()){
+                            if(!saveContents(Main.this.textAreaEditor.getText())){
                                 //mostrar mensaje de error
                                 return; //cancelar operacion
                             }
@@ -86,7 +87,23 @@ public class Main {
                     });
                 });
                 JMenuItem guardar = new JMenuItem("Guardar");
+                guardar.addActionListener(e -> {
+                    saveContents(Main.this.textAreaEditor.getText());
+                });
                 JMenuItem cerrar = new JMenuItem("Cerrar");
+                cerrar.addActionListener(e -> {
+                    if (Main.this.fileContentHasBeenEdited){ //luis non me mates :)
+                        int actionToDo = askIfSaveBeforeDoAction();
+                        if (actionToDo==OPTION_CANCEL_OPERATION) return;
+                        if (actionToDo==OPTION_SAVE_FILE) {
+                            if(!saveContents(Main.this.textAreaEditor.getText())){
+                                //mostrar mensaje de error
+                                return; //cancelar operacion
+                            }
+                        }
+                    }
+                    Main.this.ventana.dispose();
+                });
             accion.add(abrir);
             accion.add(guardar);
             accion.add(cerrar);
@@ -103,7 +120,32 @@ public class Main {
             barra.add(changeColorMode);
             ventana.setJMenuBar(barra);
 
+            this.textAreaEditor.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    textChanged();
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    textChanged();
+                }
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+                    textChanged();
+                }
+
+                private void textChanged(){
+                    if (!Main.this.fileContentHasBeenEdited){ //para que non teña que cambiar o nome da ventana cada vez que se escribe algo
+                        Main.this.fileContentHasBeenEdited=true;
+                        Main.this.updateWindowName();
+                    }
+                }
+            });
             ventana.add(new JScrollPane(this.textAreaEditor));
+
+            updateWindowName();
             ventana.setVisible(true);
 
             SwingUtilities.updateComponentTreeUI(ventana);
@@ -144,11 +186,11 @@ public class Main {
     }
 
     private void openTextFile(){
-        if (!this.fileContentHasBeenEdited){
+        if (this.fileContentHasBeenEdited){
             int actionToDo = askIfSaveBeforeDoAction();
             if (actionToDo==OPTION_CANCEL_OPERATION) return;
             if (actionToDo==OPTION_SAVE_FILE) {
-                if(!saveTextFileContents()){
+                if(!saveContents(this.textAreaEditor.getText())){
                     //mostrar mensaje de error
                     return; //cancelar operacion
                 }
@@ -162,12 +204,11 @@ public class Main {
             public boolean accept(File pathname) {
                 if (pathname.isDirectory()) return true;
 
-                String[] textFileFormats = {".txt", ".html", ".htm", ".md", ".json", ".xml", ".csv", ".log", ".css", ".js", ".java", ".sql", ".yaml", ".yml", ".php", ".rb", ".py", ".pl", ".cpp", ".h", ".c", ".swift", ".go", ".scala", ".tex", ".rtf", ".ini", ".conf", ".properties", ".bat", ".sh", ".bash", ".ts", ".twig", ".mdx", ".asciidoc", ".rst", ".htaccess"};
                 String fileName=pathname.getName();
                 int lastDotIntex=fileName.lastIndexOf(".");
                 if (lastDotIntex==-1 || lastDotIntex==fileName.length()-1) return false;
                 String extension=pathname.getName().substring(lastDotIntex);
-                return pathname.isFile() && Arrays.asList(textFileFormats).contains(extension);
+                return pathname.isFile() && Arrays.asList(VALID_TEXT_FILE_EXTENSIONS).contains(extension);
             }
 
             @Override
@@ -178,7 +219,7 @@ public class Main {
 
         chooser.setFileFilter(fileFilter);
         chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        int result=chooser.showOpenDialog(ventana);
+        int result=chooser.showOpenDialog(JOptionPane.getRootFrame());
         File selectedFile=null;
         if (result!=JFileChooser.APPROVE_OPTION){ return; }
 
@@ -186,11 +227,14 @@ public class Main {
         String fileText=getFileText(selectedFile.toPath());
         if (fileText==null){
             //mensaje de error
+            System.out.println("Error ao abrir o ficheiro");
             return;
         }
 
-        this.openedFile=selectedFile.toPath();
         this.textAreaEditor.setText(fileText);
+        this.openedFile=selectedFile.toPath();
+        this.fileContentHasBeenEdited=false;
+        updateWindowName();
     }
 
     private int askIfSaveBeforeDoAction(){
@@ -199,7 +243,7 @@ public class Main {
 
         // Mostrar el cuadro de diálogo
         int saveBeforeOpenPanel = JOptionPane.showOptionDialog(
-                ventana, // Componente padre (null para centrar en la pantalla)
+                JOptionPane.getRootFrame(), // Componente padre (null para centrar en la pantalla)
                 "Tiene cambios sin guardar, que deseas hacer?", // Mensaje
                 "Atención", // Título
                 JOptionPane.DEFAULT_OPTION, // Tipo de opciones
@@ -213,14 +257,50 @@ public class Main {
         return OPTION_CANCEL_OPERATION; //do nothing and cancel the next action
     }
 
+    private Path createNewFile(){
+        String newFileName=JOptionPane.showInputDialog("Nombre del nuevo archivo:");
+        if (!isValidFileName(newFileName)){
+            System.out.println("nombre non valido");
+            return null;
+        }
+
+        JFileChooser jFileChooser=new JFileChooser();
+        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jFileChooser.setName("Selecciona directorio de guardado");
+        if (jFileChooser.showOpenDialog(null)!=JFileChooser.APPROVE_OPTION) return null;
+
+        return jFileChooser.getSelectedFile().toPath().resolve(newFileName);
+    }
+
+    private boolean isValidFileName(String fileName){
+        if (fileName == null || fileName.isBlank()) return false;
+        if (!Pattern.matches("^[a-zA-Z0-9._-]+$", fileName)) return false;
+        if (!fileName.contains(".")) return false;
+
+        String extension = fileName.substring(fileName.lastIndexOf("."));
+        if (!Arrays.asList(VALID_TEXT_FILE_EXTENSIONS).contains(extension)) return false;
+
+        return true;
+    }
+
+    private void updateWindowName(){
+        StringBuilder texto=new StringBuilder();
+        if (fileContentHasBeenEdited) texto.append("*");
+        texto.append(openedFile!=null ? openedFile.getFileName() : "Sin título");
+        texto.append(" : Editor de texto");
+        SwingUtilities.invokeLater(()->{
+            this.ventana.setTitle(texto.toString());
+        });
+    }
+
     //Data ---------------------------------------------------------------------------------------------------------------------------------
-    private String getFileText(Path openedFile){
-        if (!Files.exists(openedFile)){
+    private String getFileText(Path fileToOpenPath){
+        if (!Files.exists(fileToOpenPath)){
             System.out.println("O ficheiro non existe");
             return null;
         }
         StringBuilder text=new StringBuilder();
-        try(BufferedReader reader=new BufferedReader(new FileReader(openedFile.toFile()))){
+        try(BufferedReader reader=new BufferedReader(new FileReader(fileToOpenPath.toFile()))){
             String line;
             while ((line=reader.readLine())!=null){
                 text.append(line);
@@ -231,7 +311,27 @@ public class Main {
         }
     }
 
-    private boolean saveTextFileContents(){
-        return false;
+    private boolean saveContents(String textContent){
+        Path fileToSave=this.openedFile;
+        if (fileToSave==null || !Files.exists(fileToSave)){
+            if ((fileToSave=createNewFile())==null) return false;
+        }
+
+        try(BufferedWriter output=new BufferedWriter(new FileWriter(fileToSave.toFile()));
+            BufferedReader readerContent=new BufferedReader(new StringReader(textContent));
+        ){
+            String line;
+            while ((line=readerContent.readLine())!=null){
+                output.write(line);
+                output.write(System.lineSeparator());
+            }
+        }catch (IOException e){
+            System.out.println("Fallito o gardar: "+e.getMessage());
+            return false;
+        }
+        this.openedFile=fileToSave;
+        this.fileContentHasBeenEdited=false;
+        updateWindowName();
+        return true;
     }
 }
